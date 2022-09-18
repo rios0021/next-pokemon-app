91a148715248cb910bbd7801ed776b02255e08a6
rios0021 <rios0021@algonquinlive.com>
2022-09-18 12:29:20 -0400

    Implemented Incremental Static Generation and Regeneration

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -24,7 +24,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         paths: names.map(name => ({
             params: {name}
         })),
-        fallback: false
+        fallback: 'blocking'
     }
 }
 
@@ -37,6 +37,15 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async ({params}) => {
     const {name} = params as {name:string};
     const pokemon =  await getPokemonInfo(name);
+
+    if(!pokemon){
+        return{
+            redirect:{
+                destination:'/',
+                permanent: false
+            }
+        }
+    }
 
     return {
         props: {
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -124,7 +124,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         //         }
         //     },
         // ],
-        fallback: false
+        fallback: "blocking"
     }
 }
 
@@ -133,11 +133,20 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
     const {id} = params as {id:string};
 
     const pokemon = await getPokemonInfo(id);
+    if(!pokemon){
+        return{
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
 
     return {
         props: {
             pokemon
-        }
+        },
+        revalidate: 86400
     }
 }
 
